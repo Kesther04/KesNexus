78f224219a51ce16efbed7accba0935136b6c9dd
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2025-08-17 17:33:17 +0100

    small screen navbar added

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,31 @@
+
+export default function HamburgerMenu ({open, setOpen}:{open: boolean, setOpen:React.Dispatch<React.SetStateAction<boolean>>}) {
+    
+    return (
+        <div
+        onClick={() => setOpen(!open)}
+        className="menu-btn relative z-50 w-6 h-6 cursor-pointer text-black dark:text-white flex items-center justify-center md:hidden"
+        >
+        <span
+            className={`menu-btn__burger relative block w-5 h-[3px] rounded-sm bg-current transition-all duration-300 ease-in-out
+            ${open ? "bg-transparent rotate-[720deg]" : ""}
+            `}
+        >
+            {/* top line */}
+            <span
+            className={`absolute top-[-8px] block w-5 h-[3px] rounded-sm bg-current transition-all duration-300 ease-in-out
+                ${open ? "rotate-45 translate-x-[5px] translate-y-[8px]" : ""}
+            `}
+            ></span>
+
+            {/* bottom line */}
+            <span
+            className={`absolute top-[8px] block h-[3px] rounded-sm bg-current transition-all duration-300 ease-in-out
+                ${open ? "w-5 rotate-[-45deg] translate-x-[4px] -translate-y-[8px]" : "w-2"}
+            `}
+            ></span>
+        </span>
+        </div>
+  );
+    
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,14 @@ import { useEffect, useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import LogoAttachment from "../assets/LogoAttachment";
 import Logo from "../assets/Logo";
+import HamburgerMenu from "./HamburgerMenu";
 
 
 // This is the header component that will be used across the application
 export default function Header () { 
     const [scrolled, setScrolled] = useState(false);
     const {theme, toggleTheme} = useTheme();
+    const [open, setOpen] = useState(false);
 
     const location = useLocation();
     let currentView = location.hash;
@@ -32,11 +34,11 @@ export default function Header () {
     
     
     return (
-        <header className={`flex items-center justify-between space-x-4 py-4 px-2 md:px-20  fixed bg-white/80 dark:bg-black/80 w-full backdrop-blur-sm z-20  ${scrolled ? "shadow-md" : ""}`}>
+        <header className={`flex items-center justify-between space-x-4 py-4 px-2 md:px-20  fixed bg-white/80 dark:bg-black/80 w-full backdrop-blur-sm z-50  ${scrolled ? "shadow-md" : ""}`}>
 
             
             {/* logo for the header */}
-            <div className="relative cursor-pointer flex items-center">
+            <div className="relative cursor-pointer flex items-center z-50">
                 <Link to="/" className="absolute flex items-center justify-center  dark:text-white"> 
                     <Logo className="w-14 h-14 text-primary dark:text-primary-dark "/> 
                     <LogoAttachment/>
@@ -44,7 +46,7 @@ export default function Header () {
             </div>
 
             {/* navigation links for the header */}
-            <ul className="nav hidden opacity-0 md:opacity-100 md:flex space-x-8 font-medium">
+            <ul className={`nav ${open ? "open" : ""}`}>
 
 
                 {
@@ -65,12 +67,15 @@ export default function Header () {
             </ul>
 
 
-            {/* theme toggler */}
-            <div className="flex items-center">
+            {/* theme toggler + Hamburger Menu*/}
+            <div className="flex items-center z-50">
                 {theme === "dark" && <Sun className="themeBtn" onClick={toggleTheme} />}
-                {theme === "light" && <Moon className="themeBtn" onClick={toggleTheme} />}
-                
+                {theme === "light" && <Moon className="themeBtn" onClick={toggleTheme} />}               
+
+                <HamburgerMenu open={open} setOpen={setOpen}/>
             </div>
+
+
         </header>
     )
 }
