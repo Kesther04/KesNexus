30b2f2fd4de8199fd95879e0f0f31a5d80ba3317
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2025-08-21 01:45:45 +0100

    ui update

--- src/pages/AllProjects.tsx
+++ src/pages/AllProjects.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import ProjectCard from "../components/ProjectCard";
 import { DEV_PROJECT_CRED } from "../data/tech";
 import { COPY_PROJECT_CRED } from "../data/copy";
+import ScrollToTop from "../../utils/ScrollToTop";
 
 
 type projects = {
@@ -56,6 +57,7 @@ export default function AllProjects () {
     console.log(currentPj);
     return (
         <>
+            <ScrollToTop />
             <Header/>
 
             <main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,13 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Services from "../components/Services";
+import ScrollToTop from "../../utils/ScrollToTop"
 // import Testimonials from "../components/Testimonials";
 
 export default function Home() {
     return (
         <>
-
+            <ScrollToTop />
 
             <Header />
             
